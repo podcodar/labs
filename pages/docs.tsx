1cc2f7bab89a53fafac39261e80017024b2e7841
Marco Antônio <ma.souza.junior@gmail.com>
2022-06-25 15:35:28 -0300

    fix: add swagger types

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -3,9 +3,7 @@ import { createSwaggerSpec } from "next-swagger-doc";
 import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
 
-const SwaggerUI = dynamic<{
-  spec: any;
-}>(import("swagger-ui-react"), { ssr: false });
+const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
   return <SwaggerUI spec={spec} />;
