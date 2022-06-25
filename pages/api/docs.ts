d3d9a00288df51e4cf880fa4f8037dbab687ace2
Marco Antônio <ma.souza.junior@gmail.com>
2022-06-25 14:44:12 -0300

    fea: add api docs

---
+++ pages/api/docs.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSwagger } from "next-swagger-doc";
+
+const swaggerHandler = withSwagger({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "NextJS Swagger",
+      version: "0.1.0",
+    },
+  },
+  apiFolder: "pages/api",
+});
+export default swaggerHandler();
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     description: Returns the hello world
+ *     responses:
+ *       200:
+ *         description: hello world
+ */
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
