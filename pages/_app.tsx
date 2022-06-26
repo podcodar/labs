266e159cff340ad3f2342e706247af62abe07a01
Marco Antônio <ma.souza.junior@gmail.com>
2022-06-26 11:15:10 -0300

    enh: change favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>PodCodar labs</title>
+        <meta name="description" content="PodCodar tech lab" />
+        <link rel="icon" href="/images/favicon.svg" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold p-4">
           PodCodar
@@ -54,11 +48,12 @@ const Home: NextPage = () => {
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://podcodar.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <p></p>
+          <Image src="/images/just-llama.png" alt="PodCodar Logo" width={20} height={20} /> PodCodar
         </a>
       </footer>
     </div>
