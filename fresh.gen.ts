b515bfb9debdc9b080a1444905cdd63d20880b65
Marco Antônio <ma.souza.junior@gmail.com>
2022-09-17 15:44:22 -0300

    fea: add project buttons on the home page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,12 +5,14 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/projects/colinha.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/_app.tsx": $0,
     "./routes/index.tsx": $1,
+    "./routes/projects/colinha.tsx": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,33 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <Head>
-        <title>PodCodar Labs 👩🏾‍🔬</title>
-      </Head>
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class="p-4 mx-auto max-w-screen-md text-center">
+      <h1 class="text-xl">
+        👩🏾‍🔬 PodCodar.<span class="font-bold">labs</span>
+      </h1>
+
+      <div class="flex gap-4 my-4 justify-center">
+        {projectLinks.map((link) => (
+          <a
+            href={link.href}
+            class="
+              text-blue-300 border-1 border-blue-200 py-2 px-8 rounded-full
+              hover:underline hover:bg-blue-50 hover:shadow
+            "
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
+
+type Link = {
+  name: string;
+  href: string;
+};
+
+const projectLinks: Link[] = [
+  { name: "Colinha", href: "/colinha" },
+  { name: "Counter", href: "/counter" },
+];
