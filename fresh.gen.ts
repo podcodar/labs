189183998ea6f540dbb0908d16f9f578644696bf
Marco Antônio <ma.souza.junior@gmail.com>
2023-06-22 18:10:12 -0300

    enh: update fresh dependencies

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/ws.ts";
 import * as $1 from "./routes/index.tsx";
 import * as $2 from "./routes/projects/counter.tsx";
@@ -20,7 +19,6 @@ const manifest = {
     "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
